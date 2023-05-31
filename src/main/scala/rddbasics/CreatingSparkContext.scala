e352d16983a25a03d52fc8a24766c91527e75005
hftamayo <hftamayo@gmail.com>
2023-05-31 11:11:04 -0600

    first version of the file lines counter

--- src/main/scala/rddbasics/CreatingSparkContext.scala
+++ src/main/scala/rddbasics/CreatingSparkContext.scala
@@ -11,6 +11,18 @@ object CreatingSparkContext {
     sparkConf.setAppName("First Spark Application")
     sparkConf.setMaster("local")
 
-  }
+    val sc = new SparkContext(sparkConf)
 
+    val array = Array(1,2,3,4,5,6,7,8,9,0)
+
+    val arrayRDD = sc.parallelize(array, 2)
+
+    println("Num of elements in RDD: ", arrayRDD.count())
+    arrayRDD.foreach(println)
+
+    val file = "sampleData.csv"
+    val fileRDD = sc.textFile(file, 5)
+    println("Num of rows in file: " + fileRDD.count())
+    println(fileRDD.first())
+  }
 }
